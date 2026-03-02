ed86558c96f6b3756e01ed0220d61a9bffa083bd
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-02 10:54:03 +0200

    feat: register new user

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 # CREATE DATABASE
 
-
 class Base(DeclarativeBase):
     pass
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
@@ -33,14 +36,34 @@ with app.app_context():
     db.create_all()
 
 
+class RegisterForm(FlaskForm):
+
+    name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Name"})
+    email = StringField('Email', validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Sign me up.')
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+
+        new_user = User(email=form.email.data, name=form.name.data, password=form.password.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('secrets', name = form.name.data))
+
+    return render_template("register.html", form = form)
 
 
 @app.route('/login')
@@ -48,9 +71,9 @@ def login():
     return render_template("login.html")
 
 
-@app.route('/secrets')
-def secrets():
-    return render_template("secrets.html")
+@app.route('/secrets/<name>')
+def secrets(name):
+    return render_template("secrets.html", name = name)
 
 
 @app.route('/logout')
